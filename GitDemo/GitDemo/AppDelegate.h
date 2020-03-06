8ab54cb76bf9d8bfb0e7cd0fbc6353f7f5944a47
张辉 <zhanghui@didachuxing.com>
2020-03-06 22:00:15 +0800

    feat(add new project): 这是新的iOS工程

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Dubhe on 2020/3/6.
+//  Copyright © 2020 Dubhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by Dubhe on 2020/3/6.
+//  Copyright © 2020 Dubhe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
